d2589fa3dc433d51f45a1b5b8bbb9f660d708146
Marina <marina.kato@student.uts.edu.au>
2025-09-03 16:40:04 +1000

    fixed filename typo, testing pipeline frontend

--- client/src/utils/index.jsx
+++ client/src/utils/index.jsx
@@ -1,7 +1,7 @@
 import Button from "./Button.jsx";
 import Input from "./Input.jsx";
 import LogoutBtn from "./LogoutBtn.jsx";
-import LoginButton from "./LoginBtn";
-import CheckoutBtn from "./CheckOutBtn.jsx";
+import LoginButton from "./LoginBtn.jsx";
+import CheckoutBtn from "./CheckoutBtn.jsx";
 
 export { Button, Input, LogoutBtn, LoginButton, CheckoutBtn };
