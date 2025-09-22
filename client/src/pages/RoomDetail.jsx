cf17a2e7ee4d00d87c13c6d67cb1a3f8d4e1d788
Marina <marina.kato@student.uts.edu.au>
2025-09-22 11:15:41 +1000

    edited RoomDetail to have default export so I could load app in browser

---
+++ client/src/pages/RoomDetail.jsx
@@ -0,0 +1,7 @@
+function RoomDetail() {
+  return <div>This is the Room Detail Page</div>;
+}
+// Marina: I just added this in so that I could load MoveNMuse 
+// to test on browser. Feel free to change
+
+export default RoomDetail;
