b3cef9b18375d69a4c3662923d47b2e2f26f7306
Marina <marina.kato@student.uts.edu.au>
2025-10-02 07:52:47 +1000

    merge changes from test branch

--- server/src/app.js
+++ server/src/app.js
@@ -44,7 +44,7 @@ app.use(
 app.use(express.json());
 app.use(express.static("public"));
 app.use(cookieParser());
-
+app.use("/api", Routes); 
 app.use("/api/rooms", roomRoutes);
 app.use("/api/courses", courseRoutes);
 app.use("/api/bookings", bookingCourseRoutes);
